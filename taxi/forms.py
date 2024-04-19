36651f429d4b15f2ab886509f4005dd4a6788810
bujhm <Bujhm99@gmail.com>
2024-04-20 01:28:55 +0300

    Don't get through the tests with user name in form

---
+++ taxi/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
+
+from taxi.models import Car
+
+
+class DriverCreationForm(UserCreationForm):
+    license_number = forms.CharField(
+        max_length=8,
+        required=True,
+        validators=[RegexValidator(
+            regex=r"[A-Z]{3}\d{5}",
+            message="It should has 3 serial big char and 5 digits",
+        )]
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = (UserCreationForm.Meta.fields
+                  + ("license_number", "first_name", "last_name",))
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields["username"].widget.attrs["readonly"] = "true"
+    #
+    #   Don't get through the tests
+    #
+    license_number = forms.CharField(
+        max_length=8,
+        required=True,
+        validators=[RegexValidator(
+            regex=r"[A-Z]{3}\d{5}",
+            message="It should has 3 serial big char and 5 digits",
+        )]
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("license_number",)
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False)
+
+    class Meta:
+        model = Car
+        fields = "__all__"
